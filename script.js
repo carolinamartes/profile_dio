b3349c793908d607c1c922a8511e05a1aac025ac
Carolina Martes <carolina.martes@gmail.com>
2020-12-13 18:09:28 +0000

    Add google analytics and diverse updates

--- script.js
+++ script.js
@@ -108,10 +108,3 @@ particlesJS("particles-js", {
     },
     "retina_detect": true
 });
-// var count_particles, update;
-// count_particles = document.querySelector('.js-count-particles');
-// update = function() {
-//     if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; }
-//     requestAnimationFrame(update);
-// };
-// requestAnimationFrame(update);;
